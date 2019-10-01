d77d1f62e74124c17af63ab3c2b6628cb58d2c06
Juan Rosario <juanelias18@hotmail.com>
2019-10-01 19:22:35 -0400

    Avanzo

---
+++ Entidades/Sugerencias.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Sugerencias
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime Fecha { get; set; }
+        public Sugerencias()
+        {
+            Id = 0;
+            Descripcion = string.Empty;
+            Fecha = DateTime.Now;
+        }
+    }
+}
