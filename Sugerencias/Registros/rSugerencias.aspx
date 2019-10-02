eca40adb1b6c9aaf88b9ecfcf4883bf980c0343d
Juan Rosario <juanelias18@hotmail.com>
2019-10-01 20:21:25 -0400

    Registro hecho

---
+++ Sugerencias/Registros/rSugerencias.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="rSugerencias.aspx.cs" Inherits="Sugerencias.Registros.rSugerencias" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="Contenido" runat="server">
+    <div class="panel-body">
+        <div class="form-horizontal col-md-12" role="form">
+            <div class="form-group row">
+                    <label for="ID" class="col-sm-1 col-form-label">ID</label>
+                    <div class="col-md-1 col-sm-2 col-xs-4">
+                        <asp:TextBox type="number" ID="IDTextBox" runat="server" min=0 class="form-control input-sm"></asp:TextBox>
+                    </div>
+                    <div class="col-md-3 col-sm-2 col-xs-4">
+                         <asp:LinkButton ID="BuscarButton" CssClass="btn btn-dark btn-block btn-md" CausesValidation="false" runat="server" Text="Buscar" OnClick="BuscarButton_Click"></asp:LinkButton>
+                    </div>    
+                    <label for="Fecha" class="col-xs-1 col-form-label">Fecha</label>
+                    <div class="col-md-3">
+                        <asp:TextBox ID="FechaTextBox" type="date" runat="server" Class="form-control input-sm"></asp:TextBox>
+                        <asp:RequiredFieldValidator ID="RFVFecha" runat="server" MaxLength="200" ControlToValidate="FechaTextBox" ErrorMessage="Campo obligatorio" ForeColor="Black" Display="Dynamic" SetFocusOnError="True" ToolTip="Campo obligatorio"></asp:RequiredFieldValidator>
+                    </div>
+             </div>
+
+             <%--Descripcion--%>
+                <div class="form-group row">
+                    <label for="Descripcion" class="col-sm-1 col-form-label">Descripcion</label>
+                    <div class="col-md-4">
+                        <asp:TextBox runat="server" ID="DescripcionTextBox" CssClass="form-control input-sm"></asp:TextBox>  
+                        <asp:RequiredFieldValidator ID="RFVDescripcion" runat="server" MaxLength="200" ControlToValidate="DescripcionTextBox" ErrorMessage="Campo obligatorio" ForeColor="Black" Display="Dynamic" SetFocusOnError="True" ToolTip="Campo obligatorio"></asp:RequiredFieldValidator>
+                    </div>
+                </div>
+        </div>
+        <br />
+           <div class="panel-footer">
+            <div class="text-center">
+                <div class="form-group" style="display: inline-block">
+
+                    <asp:Button Text="Nuevo" class="btn btn-warning btn-sm" CausesValidation="false" style="color:#fff" runat="server" ID="NuevoButton" OnClick="NuevoButton_Click" />
+                    <asp:Button Text="Guardar" class="btn btn-success btn-sm" runat="server" ID="GuardarButton" OnClick="GuardarButton_Click"/>
+                    <asp:Button Text="Eliminar" class="btn btn-danger btn-sm" CausesValidation="false" runat="server" ID="EliminarButton" OnClick="EliminarButton_Click" />
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+</asp:Content>
---
+++ Sugerencias/Registros/rSugerencias.aspx.cs
@@ -0,0 +1,124 @@
+﻿using BLL;
+using Entidades;
+using Sugerencias.Utilitarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Sugerencias.Registros
+{
+    public partial class rSugerencias : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                IDTextBox.Text = "0";
+                FechaTextBox.Text = DateTime.Now.ToString("yyyy-MM-dd");
+            }        
+        }
+        private bool ExisteEnLaBaseDeDatos()
+        {
+            RepositorioBase<Entidades.Sugerencias> Repositorio = new RepositorioBase<Entidades.Sugerencias>();
+            Entidades.Sugerencias Tipos = Repositorio.Buscar(Utils.ToInt(IDTextBox.Text));
+            return (Tipos != null);
+        }
+        private Entidades.Sugerencias LlenaClase()
+        {
+            Entidades.Sugerencias Tipos = new Entidades.Sugerencias();
+
+            Tipos.Id = Utils.ToInt(IDTextBox.Text);
+            Tipos.Descripcion = DescripcionTextBox.Text;
+            Tipos.Fecha = Utils.ToDateTime(FechaTextBox.Text);
+
+            return Tipos;
+        }
+        private void LlenaCampo(Entidades.Sugerencias Tipos)
+        {
+            IDTextBox.Text = Tipos.Id.ToString();
+            DescripcionTextBox.Text = Tipos.Descripcion;
+            FechaTextBox.Text = Tipos.Fecha.ToString("yyyy-MM-dd");
+        }
+
+        protected void NuevoButton_Click(object sender, EventArgs e)
+        {
+            Response.Redirect(Request.RawUrl);
+        }
+
+        protected void BuscarButton_Click(object sender, EventArgs e)
+        {
+            RepositorioBase<Entidades.Sugerencias> Repositorio = new RepositorioBase<Entidades.Sugerencias>();
+
+            Entidades.Sugerencias Tipos = new Entidades.Sugerencias();
+
+            Tipos = Repositorio.Buscar(Utils.ToInt(IDTextBox.Text));
+
+            if (Tipos != null)
+                LlenaCampo(Tipos);
+            else
+            {
+                Utils.ShowToastr(this.Page, "Problema al buscar", "Error", "error");
+            }
+        }
+
+        protected void GuardarButton_Click(object sender, EventArgs e)
+        {
+            Entidades.Sugerencias Tipos = new Entidades.Sugerencias();
+            RepositorioBase<Entidades.Sugerencias> Repositorio = new RepositorioBase<Entidades.Sugerencias>();
+            bool paso = false;
+
+            Tipos = LlenaClase();
+
+            if (Utils.ToInt(IDTextBox.Text) == 0)
+            {
+                paso = Repositorio.Guardar(Tipos);
+                Response.Redirect(Request.RawUrl);
+            }
+            else
+            {
+                if (!ExisteEnLaBaseDeDatos())
+                {
+
+                    Utils.ShowToastr(this.Page, "Problema al guardar", "Error", "error");
+                    return;
+                }
+                paso = Repositorio.Modificar(Tipos);
+                Response.Redirect(Request.RawUrl);
+            }
+
+            if (paso)
+            {
+                Utils.ShowToastr(this.Page, "Guardado con exito!!", "Guardado", "success");
+                return;
+            }
+            else
+                Utils.ShowToastr(this.Page, "Problema al guardar", "Error", "error");
+
+            Response.Redirect(Request.RawUrl);
+        }
+
+        protected void EliminarButton_Click(object sender, EventArgs e)
+        {
+            RepositorioBase<Entidades.Sugerencias> Repositorio = new RepositorioBase<Entidades.Sugerencias>();
+
+            var T = Repositorio.Buscar(Utils.ToInt(IDTextBox.Text));
+
+            if (T != null)
+            {
+                if (Repositorio.Eliminar(Utils.ToInt(IDTextBox.Text)))
+                {
+                    Utils.ShowToastr(this.Page, "Eliminado con exito!!", "Guardado", "success");
+                }
+                else
+                    Utils.ShowToastr(this.Page, "Problema al eliminar", "Error", "error");
+            }
+            else
+                Utils.ShowToastr(this.Page, "Problema al eliminar", "Error", "error");
+
+            Response.Redirect(Request.RawUrl);
+        }
+    }
+}
